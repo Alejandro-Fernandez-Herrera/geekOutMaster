d2a2cd3a0b90a2962fd9b2e595b1ca243d48f748
Alejandro Fernandez <103678342+Alejandro-Fernandez-Herrera@users.noreply.github.com>
2023-05-09 20:51:52 -0500

    Add files via upload

--- src/myProject/Dados.java
+++ src/myProject/Dados.java
@@ -2,29 +2,71 @@ package myProject;
 import java.util.Random;
 
 /**
- * @brief la clase Dados se encargá de generar una cara aleatoria entre las sgtes posibilidades:<br>
- * Cara de puntos con el numero 42<br>
+ * @brief la clase Dados se encargará de generar una cara aleatoria entre las sgtes posibilidades:<br>
+ * Cara de puntos con el número 42<br>
  * Los Meeples permiten relanzar los dados<br>
  * Las Naves Espaciales eliminan un dado<br>
  * Los Superhéroes revelarán el lado oculto de la cara<br>
  * Los Corazones nos brindarán un dado extra<br>
  * Los Dragones causan pérdida<br>
- * @authors Jose David Giraldo <br> Alejandro Fernandez
+ * @autors Jose David Giraldo <br> Alejandro Fernandez
  * @version v.0.1.0
  * @date 2023-04-05
  */
+
 public class Dados {
-    private int cara;
+    private int numeroCaraAccion;
+    private String funcion;
+    private String nombreDado;
+    private String activoInactivo;
 
-    /**
-     * @brief constructor de la clase dado
-     * @date 2023-04-05
-     * @version v.0.1.0
-     */
+    public Dados(){
+    }
 
-    public int getCara() {
+
+    public void setNumAccion(){
+        int numeroAleatorio;
         Random aleatorio = new Random();
-        cara = aleatorio.nextInt(6)+1;
-        return cara;
+        numeroAleatorio = aleatorio.nextInt(6)+1;
+        if(numeroCaraAccion == numeroAleatorio){
+            setNumAccion();
+        }else{
+            numeroCaraAccion = numeroAleatorio;
+        }
+    }
+
+
+    public void setNumAccionNoAleatorio(int numero){
+        numeroCaraAccion = numero;
+    }
+
+
+    public void setFuncion(String _accion){
+        funcion = _accion;
+    }
+
+
+    public void setNombreDado(String _nombre){
+        nombreDado = _nombre;
+    }
+
+
+    public void setActivoInactivo(String estado){
+        activoInactivo = estado;
+    }
+
+
+    public int getNumAccion(){
+        return numeroCaraAccion;
+    }
+
+
+    public String getFuncion(){
+        return funcion;
+    }
+
+
+    public String getNombreDado(){
+        return nombreDado;
     }
 }
